6a967fc1f8f0bbfab2d433c84293923b08f5d58f
Kacper Stępień <kacper2007x48@gmail.com>
2023-11-28 22:14:24 +0100

    create movie entity

---
+++ src/main/java/com/example/movies_api/model/Movie.java
@@ -0,0 +1,27 @@
+package com.example.movies_api.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Builder
+@AllArgsConstructor
+public class Movie {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String originalTitle;
+    private String shortDescription;
+    private String description;
+    private String youtubeTrailerId;
+    private Integer releaseYear;
+    private boolean promoted;
+    private String poster;
+}
